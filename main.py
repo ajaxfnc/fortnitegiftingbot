c0162b6d0124555e4b56315e0dc6a1fb1eff44e5
ajaxfnc <160798309+ajaxfnc@users.noreply.github.com>
2024-04-10 23:08:22 +0000

    eee

--- main.py
+++ main.py
@@ -5,4 +5,18 @@ config = None
 with open('config.json') as config_file:
     config = json.load(config_file)
 
-print(config)
+useDiscordbot = config.discord.useDiscordBot
+useDiscordDebugMode = config.discord.dbg
+fortniteAccountID = config.auth.accountId
+fortniteDeviceID = config.auth.deviceId
+fortniteSecret = config.auth.secret
+giftToAccounds = config.giftToInfo.accounts
+devSettings = config.developerSettings
+
+print(useDiscordBot)
+print(useDiscordDebugMode)
+print(fortniteAccountID)
+print(fortniteDeviceID)
+print(fortniteSecret)
+print(giftToAccounds)
+print(f"Developer Settings JSON: {developerSettings}")
