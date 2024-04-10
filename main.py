3832321415cb245f49fcf5cbf3f0382e15b21df0
ajaxfnc <160798309+ajaxfnc@users.noreply.github.com>
2024-04-10 22:59:55 +0000

    eee

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 import json
 from json import *
-config = json.loads("config.json")
+config = None
+with open('config.json') as config_file:
+    config = json.load(config_file)
 
 print(config)
